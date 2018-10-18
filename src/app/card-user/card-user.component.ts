9a547f001eca0ff3d3260dec03cef39ef316ab05
Alex-Magdich <alexkotorij@gmail.com>
2018-10-18 19:56:15 +0300

    18.10.2018

--- src/app/card-user/card-user.component.ts
+++ src/app/card-user/card-user.component.ts
@@ -2,17 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CardUserService } from './card-user.service';
 import { User } from './user';
+import { GithubService } from '../github.service';
 @Component({
   selector: 'app-card-user',
   templateUrl: './card-user.component.html',
   styleUrls: ['./card-user.component.css']
 })
 export class CardUserComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private cardUserService: CardUserService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private githubService: GithubService,
+    private cardUserService: CardUserService
+  ) {}
   user: any;
+  posts: any;
+  newPost = '';
+
+  onKey(event: any) {
+    this.newPost = event.target.value;
+  }
+  onAdd() {
+    this.githubService.addPost(this.newPost, this.user.id).subscribe(posts => (this.posts = posts));
+  }
   ngOnInit() {
-    this.route.params.subscribe(({ id }) =>
-      this.cardUserService.getUser(id).subscribe((user: User) => (this.user = user))
-    );
+    this.route.params.subscribe(({ id }) => {
+      this.cardUserService.getUser(id).subscribe((user: User) => (this.user = user));
+      this.githubService.getPostsForUser(id).subscribe(posts => (this.posts = posts));
+    });
   }
 }
